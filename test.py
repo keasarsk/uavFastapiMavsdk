3eb882f7c6cb61205bdbb71459a9f4a57203cb47
k <shaokaikkk@163.com>
2023-02-26 09:28:48 +0800

    test.py

--- test.py
+++ test.py
@@ -5,40 +5,50 @@ import time
 async def run():
     print("test yes")
     drone = System()
-    drone.connect(system_address="tcp://192.168.1.191:8080")
+    # drone.connect(system_address="tcp://192.168.1.191:8080")
+    await drone.connect(system_address="udp://:14540")
 
     print("Waiting for drone to connect...")
-    for state in drone.core.connection_state():
+    async for state in drone.core.connection_state():
         if state.is_connected:
             print(f"Drone discovered!")
             break
 
     print("Waiting for drone to have a global position estimate...")
-    for health in drone.telemetry.health():
+    async for health in drone.telemetry.health():
         if health.is_global_position_ok:
             print("Global position estimate ok")
             break
     
     # await drone.action.arm()
 
-    print("lat")
-    print(drone.telemetry.position()['latitude_deg'])
-    print("lng")
-    print(drone.telemetry.position()['longitude_deg'])
-    print("batt")
-    print(drone.telemetry.battery()['remaining_percent'])
+    # print("lat")
+    # print(await drone.telemetry.position()['latitude_deg'])
+    #print(type(drone.telemetry.position()))# <class 'async_generator'>
+    #async for i in drone.telemetry.position():
+    #	print(i.latitude_deg)# yes
+    #	print(type(i))# bool
+
     # la = drone.telemetry.position()['latitude_deg']
     # ln = drone.telemetry.position()['longitude_deg']
     # bat = drone.telemetry.battery()['remaining_percent']
-    print("------flight_mode:")
-    for flight_mode in drone.telemetry.flight_mode():
-        print("FlightMode:", flight_mode)
-    print("--------is_armed:")
-    for is_armed in drone.telemetry.armed():
-        print("Is_armed:", is_armed)
-    print("--------is_in_air:")
-    for is_in_air in drone.telemetry.in_air():
-        print("Is_in_air:", is_in_air)
+    #print("------flight_mode:")
+    # FlightMode: HOLD
+    async for flight_mode in drone.telemetry.flight_mode():
+    	print(type(flight_mode))#<enum 'FlightMode>
+    #    print("FlightMode:", flight_mode)
+    
+    #print("--------is_armed:")
+    #Is_armed: False
+    #print(type(drone.telemetry.armed()))# <class 'async_generator'>
+    #async for is_armed in drone.telemetry.armed():
+    #	print(type(is_armed))# bool
+    #	print("Is_armed:", is_armed)
+    #print("--------is_in_air:")
+    #print(type(drone.telemetry.in_air()))# <class 'async_generator'>
+    #Is_in_air: False
+    #async for is_in_air in drone.telemetry.in_air():
+    #    print("Is_in_air:", is_in_air)
 
         
 
